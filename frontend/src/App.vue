925bcf64e6c11aa3fae53bc0a14410b504c6813f
hungtech511 <vhung.webdev@gmail.com>
2026-01-21 09:24:04 +0000

    (FIX): fix short cut shift T

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -193,7 +193,7 @@ watch(
         e.stopPropagation()
         window.parent.postMessage({ type: 'next_drive:mtp_open_quick_create' }, '*')
       }
-      if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === 'z') {
+      if (e.shiftKey && e.key.toLowerCase() === 't') {
         e.preventDefault()
         e.stopPropagation()
         window.parent.postMessage({ type: 'mtp_open_quick_create_task' }, '*')
